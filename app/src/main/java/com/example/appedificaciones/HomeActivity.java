81854a61608f149b33bfe67d3b02d291b3e9a958
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2024-10-07 23:28:47 -0500

    agregando ButtonNavigation y 3 fragments (home, lista y map) vacios

--- app/src/main/java/com/example/appedificaciones/HomeActivity.java
+++ app/src/main/java/com/example/appedificaciones/HomeActivity.java
@@ -1,19 +1,69 @@
 package com.example.appedificaciones;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.appedificaciones.fragments.HomeFragment;
+import com.example.appedificaciones.fragments.ListFragment;
+import com.example.appedificaciones.fragments.MapFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager = null;
+    private FragmentTransaction fragmentTransaction = null;
+    private HomeFragment homeFragment;
+    private ListFragment listaFragment = null;
+    private MapFragment mapaFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home); // Layout de la actividad con fragments
+        setContentView(R.layout.activity_home);// Layout de la actividad con fragments
+
+        fragmentManager = getSupportFragmentManager();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setSelectedItemId(R.id.menu_home);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.menu_home) {
+                    homeFragment = HomeFragment.newInstance("", "");
+                    loadFragment(homeFragment);
+                    return true;
+                } else if (menuItem.getItemId() == R.id.menu_lista) {
+                    listaFragment = ListFragment.newInstance("", "");
+                    loadFragment(listaFragment);
+                    return true;
+                } else if (menuItem.getItemId() == R.id.menu_mapa) {
+                    mapaFragment = MapFragment.newInstance("", "");
+                    loadFragment(mapaFragment);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        if (fragmentManager != null) {
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
+            fragmentTransaction.commit();
+        }
     }
 
 }
